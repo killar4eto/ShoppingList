3d00e10f194bf947e9f7a41e5f6afdfedb2581de
killar4eto <killar4eto1992@gmail.com>
2016-12-06 20:19:32 +0100

    Adding Auth class  and some other stuff :)

--- app/src/main/java/baa/shoppinglist/AuthActivity.java
+++ app/src/main/java/baa/shoppinglist/AuthActivity.java
@@ -1,21 +1,153 @@
 package baa.shoppinglist;
 
-import android.content.Context;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AuthActivity extends AppCompatActivity {
+
+    //Firebase mAuth
+    private FirebaseAuth mAuth;
+    private ProgressDialog progressDialog;
+
+    private Button SignMe;
+    private Button RegisterMe;
+    private EditText userEmail;
+    private EditText userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
-    }
 
-    public void PickPicture(View v){
-        Context context = getApplicationContext();
-        Toast.makeText(context.getApplicationContext(), "Not working at the moment. Try again later", Toast.LENGTH_LONG).show();
+        Firebase.setAndroidContext(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(this);
+
+        SignMe = (Button) findViewById(R.id.SignMe);
+        RegisterMe = (Button) findViewById(R.id.RegisterMe);
+        userEmail = (EditText) findViewById(R.id.userEmail);
+        userPassword = (EditText) findViewById(R.id.userPassword);
+
+        RegisterMe.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                String email = userEmail.getText().toString();
+                String password = userPassword.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+
+                    Toast.makeText(getApplication().getBaseContext(), "Please enter Email.", Toast.LENGTH_LONG).show();
+                    return;
+
+                }
+                if(TextUtils.isEmpty(password)){
+
+                    Toast.makeText(getApplication().getBaseContext(), "Please enter Password.", Toast.LENGTH_LONG).show();
+                    return;
+
+                }
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressDialog.setMessage("Registering your email... Please wait!");
+                progressDialog.show();
+
+                //Create user
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(AuthActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(AuthActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(AuthActivity.this, "Authentication failed." + task.getException(), Toast.LENGTH_SHORT).show();
+                                } else {
+                                    //Save token
+                                    SharedPreferences preferences = getSharedPreferences("User_info", MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = preferences.edit();
+                                    editor.putString("isLogged", "Yes");
+                                    editor.commit();
+
+                                    startActivity(new Intent(AuthActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        });
+
+        SignMe.setOnClickListener(new View.OnClickListener() {
+
+            String email = userEmail.getText().toString();
+            String password = userPassword.getText().toString();
+
+
+            @Override
+            public void onClick(View view) {
+
+                if(TextUtils.isEmpty(email)){
+
+                    Toast.makeText(getApplication().getBaseContext(), "Please enter Email.", Toast.LENGTH_LONG).show();
+                    return;
+
+                }
+                if(TextUtils.isEmpty(password)){
+
+                    Toast.makeText(getApplication().getBaseContext(), "Please enter Password.", Toast.LENGTH_LONG).show();
+                    return;
+
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(AuthActivity.this, new OnCompleteListener<AuthResult>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                Toast.makeText(AuthActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        userPassword.setError(getString(R.string.minimum_password));
+                                    } else {
+                                        Toast.makeText(AuthActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    //Save token
+                                    SharedPreferences preferences = getSharedPreferences("User_info", MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = preferences.edit();
+                                    editor.putString("isLogged", "Yes");
+                                    editor.commit();
+
+                                    Intent intent = new Intent(AuthActivity.this, ShopList.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 }
--- app/src/main/java/baa/shoppinglist/ShopList.java
+++ app/src/main/java/baa/shoppinglist/ShopList.java
@@ -1,7 +1,10 @@
 package baa.shoppinglist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class ShopList extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class ShopList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_list);
+    }
+
+    public void SignOut(View v){
+        //Save token
+        SharedPreferences preferences = getSharedPreferences("User_info", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("isLogged", "");
+        editor.commit();
+
+        startActivity(new Intent(ShopList.this, MainActivity.class));
+        finish();
     }
 }
